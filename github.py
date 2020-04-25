7622d4cfa9ccfaa84374e6e357c7dc8840097ac6
Puneeth456 <shivanpuneeth50@gmail.com>
2020-04-25 15:28:16 +0530

    first commit

---
+++ github.py
@@ -0,0 +1,13 @@
+Git hub:
+
+Repository: Central location where entire source code atrifcats placed
+
+Source code is available to the entire team
+version can be mainted
+at any point of time we can go back to the older version
+
+
+
+git hub
+bit backet
+git callable(svn)
