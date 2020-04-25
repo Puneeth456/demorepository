b9ae16190c5f3f65feaa348ad9ad2ba70f5ee290
Puneeth456 <shivanpuneeth50@gmail.com>
2020-04-25 15:38:06 +0530

    second commit

--- github.py
+++ github.py
@@ -1,13 +1,30 @@
-Git hub:
+# Git hub:
+#
+# Repository: Central location where entire source code atrifcats placed
+#
+# Source code is available to the entire team
+# version can be mainted
+# at any point of time we can go back to the older version
+#
+#
+#
+# git hub
+# bit backet
+# git callable(svn)
+#
 
-Repository: Central location where entire source code atrifcats placed
+#
+# x=20
+# def f1():
+#     global x
+#     x=10
+#     print(" functipn",x)
+#
+#
+# f1()
+#
 
-Source code is available to the entire team
-version can be mainted
-at any point of time we can go back to the older version
+
+logs in python
 
 
-
-git hub
-bit backet
-git callable(svn)
